6ecf2288a240e8dc0356a2a199ebadeec93c311c
Hoang Tran <trhoang2003@gmail.com>
2024-02-10 21:15:27 -0800

    1st Project complete
    Finish static page

--- Testing/Setup/src/main.jsx
+++ Testing/Setup/src/main.jsx
@@ -1,20 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-const navbar = (
-  <nav>
-      <h1>Bob's Bistro</h1>
-      <ul>
-          <li>Testing</li>
-          <li>About</li>
-          <li>Contact</li>
-      </ul>
-  </nav>
-)
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import "./index.css"
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  navbar
-)
+ReactDOM.render(<App />, document.getElementById("root"))
