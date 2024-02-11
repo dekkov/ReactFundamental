c14f4b3bc996d6a4292eb5abeb4619535e018bba
Hoang Tran <trhoang2003@gmail.com>
2024-02-10 22:03:26 -0800

    Update workspace

--- Testing/Setup/src/resources/Navbar.jsx
+++ Testing/Setup/src/resources/Navbar.jsx
@@ -6,7 +6,6 @@ export default function Navbar() {
     return (
         <nav>
             <img src={reactLogo} className="logo react" alt="React logo" />
-
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
