70cbeee9a559c0b066ceea70cdbf1b3bbebe314f
Hoang Tran <trhoang2003@gmail.com>
2024-02-11 15:55:31 -0800

    Added new data
    Added data.jsx and new images

--- Project-2/airbnb-clone/src/components/Navbar.jsx
+++ Project-2/airbnb-clone/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src={'../../public/images/airbnb-logo.png'} className="logo react" alt="React logo" />
+            <img src={'../images/airbnb-logo.png'} className="logo react" alt="React logo" />
 
         </nav>
     )
